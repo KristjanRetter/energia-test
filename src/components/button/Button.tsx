d514c299cd38cd639fb8a786ef47912ccdff537a
unknown <kristjan.retter@outlook.com>
2020-12-04 19:16:00 +0200

    button component

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,21 @@
+import React, { ReactChild } from 'react';
+import './Button.sass';
+
+interface ButtonProps {
+  type?: 'secondary' | 'danger' | 'icon';
+  disabled?: boolean;
+  onClick: () => void;
+  children: ReactChild;
+}
+
+export default function Button({ type, disabled, onClick, children }: ButtonProps) {
+  return (
+    <button disabled={disabled} className={disabled ? 'button-disabled' : type ? `button-${type}` : 'button'} onClick={() => onClick()}>
+      {children}
+    </button>
+  );
+}
+
+Button.defaultProps = {
+  disabled: false,
+};
--- src/pages/service/Service.tsx
+++ src/pages/service/Service.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
+import Button from '../../components/button/Button';
 import Container from '../../components/container/Container';
 import Header from '../../components/header/Header';
+import CloseIcon from '../../assets/close-icon.svg';
 
 export default function Service() {
   return (
     <Container>
-      <Header title='Service' description='Click on the products picture to add it on the bill.'>
-        <button>ssss</button>
-      </Header>
+      <div>
+        <Header title='Service' description='Click on the products picture to add it on the bill.'>
+          <Button type='secondary' onClick={() => console.log('sss')}>
+            Edit
+          </Button>
+        </Header>
+
+        <Button onClick={() => console.log('sss')}>primary</Button>
+        <Button disabled={true} onClick={() => console.log('sss')}>
+          disabled
+        </Button>
+        <Button type='danger' onClick={() => console.log('sss')}>
+          danger
+        </Button>
+        <Button type='icon' onClick={() => console.log('sss')}>
+          <img src={CloseIcon} />
+        </Button>
+      </div>
     </Container>
   );
 }
