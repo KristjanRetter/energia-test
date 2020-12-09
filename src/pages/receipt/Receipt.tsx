b180983775ad2b650034c22be6422cdd8b069e75
unknown <kristjan.retter@outlook.com>
2020-12-09 15:33:18 +0200

    refactor

--- src/pages/receipt/Receipt.tsx
+++ src/pages/receipt/Receipt.tsx
@@ -1,48 +1,34 @@
-import React from 'react';
-import Button from '../../components/button/Button';
+import React, { useEffect, useState } from 'react';
 import './Receipt.sass';
-import Container from '../../components/container/Container';
 import { ReceiptTemplate } from '../service/components/checkout-modal/ReceiptTemplate';
-import { pdf } from '@react-pdf/renderer';
+import { PDFDownloadLink } from '@react-pdf/renderer';
 import { useParams } from 'react-router-dom';
 import { getDocument } from '../../common/api';
+import Button from '../../components/button/Button';
 
 export default function Receipt() {
   const { id }: any = useParams();
+  const [receiptData, setReceiptData] = useState(null);
 
-  const saveBlob = (blob: any, filename: any) => {
-    const a = document.createElement('a');
-    document.body.appendChild(a);
-    a.style.display = 'none';
-    const url = window.URL.createObjectURL(blob);
-    a.href = url;
-    a.download = filename;
-    a.click();
-    window.URL.revokeObjectURL(url);
-  };
-
-  const savePdf = async (document: any, filename: any) => {
-    saveBlob(await pdf(document).toBlob(), filename);
-  };
-
-  const handleDownload = () => {
-    console.log(id);
+  useEffect(() => {
     getDocument('receipt', id)
       .then(receipt => {
         console.log(receipt);
-        const info = receipt.data();
-        console.log(receipt.exists);
-        if (receipt.data()) {
-          console.log(receipt.data());
-          savePdf(<ReceiptTemplate data={receipt.data()} />, `receipt-${id}.pdf`);
+        if (receipt.exists) {
+          const response: any = receipt.data();
+          setReceiptData(response);
         }
       })
       .catch(error => console.log(error));
-  };
+  }, []);
 
   return (
     <div className='receipt-container'>
-      <Button onClick={() => handleDownload()}>Download receipt</Button>
+      {receiptData && (
+        <PDFDownloadLink document={<ReceiptTemplate data={receiptData} />} fileName={'receipt-' + id + '.pdf'}>
+          <Button> Download receipt</Button>
+        </PDFDownloadLink>
+      )}
     </div>
   );
 }
